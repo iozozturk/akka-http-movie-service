9814d49e08849faefc0bbc0c339b3e210d390143
İsmet Özöztürk <ismet.ozozturk@eu.sony.com>
2017-04-16 12:28:31 +0300

    Logging support and basic route definitions

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,29 +1,53 @@
+import akka.actor.ActorSystem
+import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.Directives._
+import akka.stream.{ActorMaterializer, Materializer}
+import models.Movie
+import spray.json.DefaultJsonProtocol
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, ExecutionContextExecutor}
 import scala.concurrent.duration.DurationLong
 
-object Application { //extends UlakSystem {
 
-  def main(args: Array[String]): Unit = {
-    val route =
-      path("movies") {
+trait Protocols extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val movieFormat = jsonFormat4(Movie)
+}
+
+trait MovieService extends Protocols {
+  implicit val system: ActorSystem
+  implicit val executor: ExecutionContextExecutor
+  implicit val materializer: Materializer
+  val logger: LoggingAdapter
+
+  val route =
+    path("movies") {
+      post {
+        entity(as[Movie]) { movie =>
+          logger.info(s"Registering movie: ${movie.title}")
+          complete(s"Movie: ${movie.title} registered")
+        }
+      }
+    } ~
+      path("reservations") {
         post {
           ???
         }
-        get {
-          ???
-        }
-      } ~
-        path("reservations") {
-          post {
-            ???
-          }
-        }
+      }
+}
 
-//    val binding = Await.result(Http().bindAndHandle(route, "0.0.0.0", 9000), 3.seconds)
+object Application extends MovieService {
+  override implicit val system: ActorSystem = ActorSystem()
+  override implicit val executor: ExecutionContextExecutor = system.dispatcher
+  override implicit val materializer: ActorMaterializer = ActorMaterializer()
+  override val logger = Logging(system, getClass)
+
+  def main(args: Array[String]): Unit = {
+
+    val binding = Await.result(Http().bindAndHandle(route, "0.0.0.0", 9000), 3.seconds)
 
     println("Started server at 0.0.0.0:9000")
   }
+
 }
