523566d8ed8cfaa77b0ca5d4c89878db088cf7f3
İsmet Özöztürk <ismet.ozozturk@eu.sony.com>
2017-04-19 22:04:31 +0300

    Movie case class constructor refactoring tests

--- src/test/scala/repos/MovieRepoTest.scala
+++ src/test/scala/repos/MovieRepoTest.scala
@@ -2,21 +2,33 @@ package repos
 
 import models.Movie
 import org.scalatest.{AsyncWordSpec, Matchers}
+import play.api.libs.json.{JsObject, Json}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.{DurationLong, FiniteDuration}
 
-class MovieRepoTest extends AsyncWordSpec with Matchers  {
+class MovieRepoTest extends AsyncWordSpec with Matchers {
   val atMost: FiniteDuration = 10.seconds
 
   "MovieRepo" should {
+    val movie = Movie(Json.parse(
+      """
+        |{
+        | "_id": "id",
+        | "imdbId": "tt01",
+        | "title": "title",
+        | "availableSeats": 1,
+        | "screenId": "screenId"
+        |}
+      """.stripMargin).as[JsObject])
+
     "insert movie to db" in {
-      Await.ready(MovieRepo.createMovie(Movie("id", "title", 1, "screenId")), atMost).map(s => assert(true))
+      Await.ready(MovieRepo.createMovie(movie), atMost).map(s => assert(true))
     }
     "find movie from db" in {
-      Await.ready(MovieRepo.createMovie(Movie("id", "title", 1, "screenId")), atMost)
+      Await.ready(MovieRepo.createMovie(movie), atMost)
       Await.ready(MovieRepo.findMovie("id"), atMost).map { movie =>
-        movie.imdbId shouldEqual "id"
+        movie.imdbId shouldEqual "tt01"
       }
     }
   }
