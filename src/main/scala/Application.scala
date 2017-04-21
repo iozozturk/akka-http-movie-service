6cc67f944004a5b2f2f08f3500ca1fd6ed8e31f0
İsmet Özöztürk <ismet.ozozturk@eu.sony.com>
2017-04-21 17:45:10 +0300

    Move MovieService into its own file

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,56 +1,9 @@
-import java.util.UUID
-
-import actors._
 import akka.event.Logging
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.HttpEntity
-import akka.http.scaladsl.model.StatusCodes._
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.unmarshalling.Unmarshaller
-import akka.pattern.ask
-import akka.util.Timeout
-import common.{ActorRegistry, MovieSystem}
-import models.Movie
-import play.api.libs.json.{JsObject, JsString, Json}
-import spray.json.DefaultJsonProtocol
+import services.MovieService
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationLong
-
-protected trait Protocols extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val um: Unmarshaller[HttpEntity, JsObject] = {
-    Unmarshaller.byteStringUnmarshaller.mapWithCharset { (data, charset) =>
-      Json.parse(data.toArray).as[JsObject]
-    }
-  }
-}
-
-protected trait MovieService extends Protocols with MovieSystem {
-  private val movieActor = ActorRegistry.movieActor
-  implicit val timeout = Timeout(10 seconds)
-
-  val route: Route =
-    path("movies") {
-      post {
-        entity(as[JsObject]) { movieJson =>
-          val movie = Movie(movieJson + ("_id" -> JsString(UUID.randomUUID().toString)))
-          logger.info(s"Movie: ${movie.title} register requested")
-          val registerResult = movieActor ? RegisterMovie(movie)
-          onSuccess(registerResult.map {
-            case RegisterSuccess => complete(OK, s"Movie: ${movie.title} registered")
-            case RegisterError => complete(InternalServerError, s"Movie: ${movie.title} not registered")
-          }) { res => res }
-        }
-      }
-    } ~
-      path("reservations") {
-        post {
-          ???
-        }
-      }
-}
 
 object Application extends MovieService {
   val logger = Logging(system, getClass)
---
+++ src/main/scala/services/MovieService.scala
@@ -0,0 +1,52 @@
+package services
+
+import java.util.UUID
+
+import actors.{RegisterError, RegisterMovie, RegisterSuccess}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.StatusCodes.{InternalServerError, OK}
+import akka.http.scaladsl.server.Directives.{as, complete, entity, onSuccess, path, post, _}
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.unmarshalling.Unmarshaller
+import akka.pattern.ask
+import akka.util.Timeout
+import common.{ActorRegistry, MovieSystem}
+import models.Movie
+import play.api.libs.json.{JsObject, JsString, Json}
+import spray.json.DefaultJsonProtocol
+
+import scala.concurrent.duration.DurationInt
+
+protected trait Protocols extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val um: Unmarshaller[HttpEntity, JsObject] = {
+    Unmarshaller.byteStringUnmarshaller.mapWithCharset { (data, charset) =>
+      Json.parse(data.toArray).as[JsObject]
+    }
+  }
+}
+
+trait MovieService extends Protocols with MovieSystem {
+  private val movieActor = ActorRegistry.movieActor
+  implicit val timeout = Timeout(10 seconds)
+
+  val route: Route =
+    path("movies") {
+      post {
+        entity(as[JsObject]) { movieJson =>
+          val movie = Movie(movieJson + ("_id" -> JsString(UUID.randomUUID().toString)))
+          logger.info(s"Movie: ${movie.title} register requested")
+          val registerResult = movieActor ? RegisterMovie(movie)
+          onSuccess(registerResult.map {
+            case RegisterSuccess => complete(OK, s"Movie: ${movie.title} registered")
+            case RegisterError => complete(InternalServerError, s"Movie: ${movie.title} not registered")
+          }) { res => res }
+        }
+      }
+    } ~
+      path("reservations") {
+        post {
+          ???
+        }
+      }
+}
